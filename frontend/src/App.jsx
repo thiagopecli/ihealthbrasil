b61fb1b10de5c2447e20e35bde2cf0031d710182
Emanuel Vittor <emanuelvittor385@gmail.com>
2026-04-06 16:43:20 -0300

    Header com seus contúdos, formatos e etc.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,65 @@
+import './App.css'
+import logoImg from './assets/Logo_ConnectHub_Branca.svg'
+import {Search, ShoppingCart, MapPin, ChevronDown,} from 'lucide-react'
+
+function App() {
+  return (
+    <div className='app-container'>
+      <header className='main-header'>
+        <div className='logo-area'>
+          <img src={logoImg} alt='iHealth Brasil' className='logo-img'/>
+            <div className='logo-text'>
+              <strong>ConnectHub</strong>
+              <span>Onde tecnologia e natureza se encontram</span>
+            </div>
+        </div>
+        <div className='search-bar'>
+          <Search className='search-icon' size={20}/>
+          <input type="text" placeholder='Pesquisar'/>
+        </div>
+        <div className='user-menu'>
+          <button className='login-btn'>Entrar</button>
+          <button className='cart-btn'>
+            <ShoppingCart size={20}/>
+            Seu carrinho
+          </button>
+        </div>
+      </header>
+
+      <div className='location-bar'>
+        <div className='delivery-info'>
+          <span>
+            Entregar em:
+          </span>
+              <strong>
+                São Paulo, SP
+                <MapPin size={20}/>
+                <ChevronDown size={20}/>
+              </strong>
+        </div>
+        <nav className='category-menu'>
+          <a href="#">Medicamentos</a>
+          <a href="#">Bem-Estar</a>
+          <a href="#">Naturais</a>
+          <a href="#">Promoções</a>
+        </nav>
+      </div>
+
+      <main className='content'>
+        <div className='content-wrapper'>
+          <section className='main-banner'>
+            <h2>[imagem do banner aqui]</h2>
+          </section>
+          <section className='categories-preview'>
+          </section>
+        </div>
+        
+        <div className='content-wrapper'>
+          <h3>O que você procura?</h3>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default App
